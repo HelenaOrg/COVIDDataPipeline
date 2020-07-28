9ee3ab80f2540a565e99049e8d4737bc8f8695aa
Will Jack <will@remedymedical.com>
2020-07-28 00:59:31 -0700

    edits to load_sd.sh

--- load_sd.sh
+++ load_sd.sh
@@ -1,10 +1,10 @@
-mkdir data/social_distancing
+mkdir -p data/social_distancing
 mkdir data/transactions
 mkdir data/weekly_patterns
 aws s3 sync s3://sg-c19-response/social-distancing/v2/ ./data/social-distancing/ --endpoint https://s3.wasabisys.com ;
 aws s3 sync s3://sg-c19-response/transactions-facteus/ ./data/transactions/ --endpoint https://s3.wasabisys.com ;
 aws s3 sync s3://sg-c19-response/weekly-patterns/v2/ ./data/weekly_patterns/ --endpoint https://s3.wasabisys.com ;
 aws s3 sync s3://sg-c19-response/weekly-patterns-delivery/weekly/ ./data/weekly_patterns/v2/ --endpoint https://s3.wasabisys.com
-python -m pip install censusgeocode ciso8601 tqdm;
-python -m pip install "dask[dataframe]" --upgrade ;
-python ./social_distancing.py
+pip3 install censusgeocode ciso8601 tqdm;
+pip3 install "dask[dataframe]" --upgrade ;
+python3 ./social_distancing.py
